befe22f040913c769d12ccca83fe1ef3287d2d8c
alihernandez <ali.hernandez9two@gmail.com>
2023-05-11 23:11:58 -0600

    super basic layout next js app

---
+++ photo_portfolio/comps/Navbar.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="https://static.wixstatic.com/media/aebaa7_090fb1a2b5d34c418af64c8ebab58b76.png/v1/fill/w_196,h_196,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/aebaa7_090fb1a2b5d34c418af64c8ebab58b76.png"></img>
+        <Link href="home">Home</Link>
+        <a>Portraits</a>
+        <a>Landscape</a>
+        <a>Other Works</a>
+        
+      </div>
+      <h2 className="navtitle"><b>Kingsland Photography</b></h2>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ photo_portfolio/src/pages/index.js
@@ -0,0 +1,51 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Kingsland Photography</title>
+        <meta name="description" content="Kingsland Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&family=Mulish:ital,wght@0,300;0,700;1,300&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+<div className="home">
+  <div className="blob"></div>
+<div className="bio">
+     
+     <span>PICTURE HERE</span>
+     <h1>Haley Abrams</h1>
+     <article>
+       Born Fort Collins, Colorado, USA, 1993 2015 Bachelors in Fine Arts
+       from The School of the Art Institute of Chicago Haley Abrams is a
+       commercial photographer and videographer born and raised in Fort
+       Collins, Colorado. She moved to Chicago in 2012 to attend The School
+       of the Art Institute and will complete her Bachelors in Fine Arts in
+       May of 2015. She combines her artistic eye with her talent for
+       commercial-based shooting to create outstanding and unusual images.
+       Her interest in photography was sparked at the young age of 12 years.
+       A combination of community participation as well as enrollment in AP
+       Art in high school aided in her admission to the School of the Art
+       Institute of Chicago with a portfolio based merit scholarship. Since
+       attendance at SAIC, she has focused her portfolio work primarily on
+       commercial projects and portraiture. Her work intends to redefine the
+       reputation of commercial photography by creating images that
+       consciously reflect artistic techniques to create advertisements as
+       high art. She focuses on shooting on location in exotic places as well
+       as in the studio, incorporating sophisticated props and materials to
+       create stunning images that go beyond traditional advertising.
+     </article>
+   </div>
+</div>
+      
+
+      
+    </>
+  );
+}
